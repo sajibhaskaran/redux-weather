9fdfe4c7f8c271c86135f24cca26df296a801e28
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-04-23 22:56:43 -0700

    react-sparklines added

---
+++ src/components/spark_line.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+
+export default (props) => {
+
+  return (
+    <div>
+    <Sparklines data={props.data} width={100} height={120}>
+    <SparklinesLine color={props.color} />
+    </Sparklines>
+    </div>
+  )
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/spark_line'
+
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure)
+    const humidity = cityData.list.map(weather => weather.main.humidity)
     return(
-      <tr>
-        <td>{cityData.city.name}</td>
+      <tr key = {name}>
+          <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+        <td>
+          <Chart data={pressure} color="blue" />
+        </td>
+        <td>
+          <Chart data={humidity} color="red" />
+        </td>
+
       </tr>
     );
   }
